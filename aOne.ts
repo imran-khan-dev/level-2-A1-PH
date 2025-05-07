f5efb764b082d8b88106d6082296113f7988bdac
imran-khan-dev <emrankhanniloy@gmail.com>
2025-05-07 22:56:21 +0600

    problem 5 done

--- aOne.ts
+++ aOne.ts
@@ -20,3 +20,38 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.reduce((acc, curr) => [...acc, ...curr], []);
 }
 
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo() {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  getModel() {
+    return `Model: ${this.model}`;
+  }
+}
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+
+
