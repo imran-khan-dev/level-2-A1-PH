3ab6c5403f5ae7cda6583d53722f04785c15adba
imran-khan-dev <emrankhanniloy@gmail.com>
2025-05-07 23:44:47 +0600

    all problems solved

--- aOne.ts
+++ aOne.ts
@@ -54,4 +54,49 @@ function processValue(value: string | number): number {
   }
 }
 
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length == 0) {
+    return null;
+  } else {
+    const result = products.reduce((max, curr) =>
+      curr.price > max.price ? curr : max
+    );
+    return result;
+  }
+}
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  }
+  return "Weekday";
+}
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (n < 0) {
+      reject("Error negative numberis not allowed");
+    } else {
+      setTimeout(() => {
+        const result = n * n;
+        resolve(result);
+      }, 1000);
+    }
+  });
+}
 
